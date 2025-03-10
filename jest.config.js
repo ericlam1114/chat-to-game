d4a9deff0b0dbf20c50f2c6887598e2b762e1fc7
ericlam1114 <83894250+ericlam1114@users.noreply.github.com>
2025-03-10 09:03:29 -0700

    Simplify testing configuration by removing Babel and Jest transform settings

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,5 @@
 // jest.config.js
 module.exports = {
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  transformIgnorePatterns: [
-    '/node_modules/(?!three)'
-  ]
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
 };
